052ccdd3c7a73d4912958b736e136807644d5629
Christina Leuci <christinaleuci@gmail.com>
2014-12-07 13:45:33 -0500

    changed footer to curate, changed session to create wardrobe upon user first intialization

--- app/controllers/api/v1/wardrobe_controller.rb
+++ app/controllers/api/v1/wardrobe_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::WardrobeController < Api::ApiController
   include ActionController::MimeResponds
   def index
     respond_to do |format|
-      # the specific wardrobe for that user
-      @wardrobe = Wardrobe.where(["id = ?", id]).select("id").first
+      @wardrobe = Wardrobe.where(["user_id = ?", 1])
       format.json { render json:@wardrobe}
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.from_omniauth(env["omniauth.auth"])
+    @wardrobe = @user.wardrobe
+    unless @wardrobe
+      @wardrobe = Wardrobe.new
+      @wardrobe.save!
+      @user.wardrobe = @wardrobe
+    end
     session[:current_user] = @user
     session[:user_id] = @user.id
     redirect_to wardrobe_index_path 
