cde69ed5fac4a9ebcda8388b288d62326e5c7f75
Christina Leuci <christinaleuci@gmail.com>
2014-12-07 19:27:15 -0500

    added user information to the user table

--- db/migrate/20141102170624_create_users.rb
+++ db/migrate/20141102170624_create_users.rb
@@ -8,6 +8,15 @@ class CreateUsers < ActiveRecord::Migration
       t.string :image
       t.string :oauth_token
       t.datetime :oauth_expires_at
+      t.string :height
+      t.integer :weight
+      t.integer :age
+      t.integer :waist_size
+      t.integer :inseam
+      t.string :preferred_pants_fit
+      t.string :shirt_size
+      t.string :preferred_shirt_fit
+      t.string :shoe_size
 
       t.timestamps
     end
