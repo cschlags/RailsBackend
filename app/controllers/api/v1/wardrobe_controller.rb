39b1bbcb1c8b0245c237bd09f99dc0e52e05d12c
Christina Leuci <christinaleuci@gmail.com>
2014-12-08 14:36:12 -0500

    showing all wardrobes

--- app/controllers/api/v1/wardrobe_controller.rb
+++ app/controllers/api/v1/wardrobe_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::WardrobeController < Api::ApiController
   include ActionController::MimeResponds
   def index
     respond_to do |format|
-      @wardrobe = Wardrobe.find(this.user.id)
+      @wardrobe = Wardrobe.all
       format.json { render json:@wardrobe}
     end
   end
