42e2e7bb8a67f417dba0ce606aa8222096971491
Christina Leuci <christinaleuci@gmail.com>
2014-12-08 14:53:13 -0500

    beginning of the wardrobe api

--- app/controllers/api/v1/wardrobe_controller.rb
+++ app/controllers/api/v1/wardrobe_controller.rb
@@ -6,4 +6,12 @@ class Api::V1::WardrobeController < Api::ApiController
       format.json { render json:@wardrobe}
     end
   end
+
+  def show
+    respond_to do |format|
+      @user = User.find(params[:id])
+      @wardrobe = Wardrobe.find(id = @user.id)
+      format.json { render json:@wardrobe }
+    end
+  end
 end
