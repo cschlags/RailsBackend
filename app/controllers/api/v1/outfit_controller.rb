50beef10a5e0a98ef2ffb6bd3af121ac887c23e3
Christina Leuci <christinaleuci@gmail.com>
2014-12-08 14:55:13 -0500

    outfit api

--- app/controllers/api/v1/outfit_controller.rb
+++ app/controllers/api/v1/outfit_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::OutfitController < Api::ApiController
   include ActionController::MimeResponds
+  
   def index
     respond_to do |format|
-      @outfits = Outfit.where(["user_id = ?", 1])
+      @outfits = Outfit.all
       format.json { render json:@outfits}
+    end
+  end
+
+  def show
+    respond_to do |format|
+      @user = User.find(params[:id])
+      @outfit = Outfit.find(id = @user.id)
+      format.json { render json:@outfit }
     end
   end
 end
