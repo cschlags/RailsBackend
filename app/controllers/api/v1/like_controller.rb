ab0094d37f1199602a4e8af2e6f056580db15161
Christina Leuci <christinaleuci@gmail.com>
2014-12-08 14:56:30 -0500

    like api

--- app/controllers/api/v1/like_controller.rb
+++ app/controllers/api/v1/like_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::LikeController < Api::ApiController
   include ActionController::MimeResponds
+
   def index
     respond_to do |format|
-      @likes = Like.where(["user_id = ?", 1])
+      @likes = Like.all
       format.json { render json:@likes}
+    end
+  end
+
+  def show
+    respond_to do |format|
+      @user = User.find(params[:id])
+      @like = Like.find(id = @user.id)
+      format.json { render json:@like }
     end
   end
 end
