4633417b8b5e0edcf3216dbe1b0a4e3612453d9e
Christina Leuci <christinaleuci@gmail.com>
2014-12-10 20:40:41 -0500

    curate user id

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ActiveRecord::Base
   has_one :like
   has_one :outfit
   has_one :wardrobe
-  before_save :change_curate_user_id
   after_create :create_wardrobe, :create_outfit, :create_like
   serialize :preferences
   
   def self.from_omniauth(auth_hash)
     where(auth_hash.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.curate_user_id = "curate"+rand(9).to_s+rand(9).to_s+rand(9).to_s+
+    rand(9).to_s+rand(9).to_s
       user.provider = auth_hash.provider
       user.uid = auth_hash.uid
       user.name = auth_hash.info.name
@@ -18,10 +19,5 @@ class User < ActiveRecord::Base
       user.preferences = { height: nil, weight: nil, age: nil, waist_size: nil, inseam: nil, preferred_pants_fit: nil, shirt_size: nil, preferred_shirt_fit: nil, shoe_size: nil}
       user.save!
     end
-  end
-
-  def change_curate_user_id
-    self.curate_user_id = "curate"+rand(9).to_s+rand(9).to_s+rand(9).to_s+
-    rand(9).to_s+rand(9).to_s
   end
 end
