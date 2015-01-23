1a94ac5d2138ff042ff77c28e420350b16e1c019
Christina Leuci <christinaleuci@gmail.com>
2015-01-23 14:28:20 -0500

    fucking work

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
-gem "aws-sdk"
-gem 'aws-s3'
+require "aws-sdk"
+require 'aws-s3'
 AWS::S3::Base.establish_connection!(
  :access_key_id   => ENV['AKIAIES3KIS3P7UPOU2A'],
  :secret_access_key => ENV['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']
