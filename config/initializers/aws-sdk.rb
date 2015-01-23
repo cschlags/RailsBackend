a8bfafb0ae2d11bebce1550bb34a9ac36b9d5298
Christina Leuci <christinaleuci@gmail.com>
2015-01-23 14:25:34 -0500

    /

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require 'aws-s3'
+require 'aws/s3'
 AWS::S3::Base.establish_connection!(
  :access_key_id   => ENV['AKIAIES3KIS3P7UPOU2A'],
  :secret_access_key => ENV['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']
