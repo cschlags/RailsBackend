0aeefc03e491ddfc8c9c001c82fadff9f16be37d
Christina Leuci <christinaleuci@gmail.com>
2015-01-23 14:26:30 -0500

    gems

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
-require 'aws/s3'
+gem "paperclip"
+gem "aws-sdk"
+require 'aws-s3'
 AWS::S3::Base.establish_connection!(
  :access_key_id   => ENV['AKIAIES3KIS3P7UPOU2A'],
  :secret_access_key => ENV['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']
