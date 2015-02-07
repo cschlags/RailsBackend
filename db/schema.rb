01aa97a74e6b7ac558351029b88190d09d83d269
Christina Leuci <christinaleuci@gmail.com>
2015-02-07 14:55:40 -0500

    user password

--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,10 @@ ActiveRecord::Schema.define(version: 20150207181809) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "preferences"
-    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
