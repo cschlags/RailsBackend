c2116c5b24b8ce457ba4ec188a2f98e4b69df1f3
Christina Leuci <christinaleuci@gmail.com>
2015-02-11 15:33:21 -0500

    token change

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,27 +1,31 @@
+require 'net/http'
 class Api::V1::TokensController  < ApplicationController
     respond_to :json
-    OAUTH_TOKEN = "10152951075729059"
+    TOKEN = "123"
     def create
-      binding.pry
-      @oauth_token = params[:oauth_token]
+      @token = params[:token]
       if request.format != :json
         render :status=>406, :json=>{:message=>"The request must be json my brony"}
         return
        end
  
-    if @oauth_token.nil?
-      logger.info("User #{@oauth_token} failed signin, user cannot be found.")
-      render :status=>400,
-             :json=>{:message=>"Yo man this is nil yo"}
-      return
-    elsif @oauth_token = OAUTH_TOKEN
-      logger.info("User #{@oauth_token} succeeded signin, they in da' system yo.")
-      render :status=>200,
-             :json=>{:message=>"Yeah bro this is cool yo"}
-      redirect_to "/application/index"
-      return
+      if @token.nil?
+        logger.info("User #{@token} failed signin, user cannot be found.")
+        render :status=>400,
+               :json=>{:message=>"Yo man this is nil yo"}
+        return
+      elsif @token = TOKEN
+        # facebook_feed = fb_oauth_search(@token)
+
+        # if facebook_feed.uid != nil
+          logger.info("User #{@token} succeeded signin, they in da' system yo.")
+          render :status=>200,
+                 :json=>{:message=>"Yeah bro this is cool yo"}
+          return
+        # else
+        # end  
+      end
     end
-  end
  
   def destroy
     render :json=>{:message=>"You've hit the token destroy page"}
@@ -34,4 +38,16 @@ class Api::V1::TokensController  < ApplicationController
       render :status=>200, :json=>{:token=>params[:id]}
     end
   end
+
+  private
+    def fb_oauth_search(access_token)
+      binding.pry
+      url = URI.parse('https://graph.facebook.com/me?fields=id&access_token='+@accesstoken)
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      binding.pry
+      puts res.body
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ActiveRecord::Base
 
   
   def self.from_omniauth(auth)
+    binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
