cad17d8528c2d61560b6404c761fa282b88c850f
Christina Leuci <christinaleuci@gmail.com>
2015-02-24 15:50:02 -0500

    edit user.wardrobe

--- app/controllers/api/v1/wardrobe_controller.rb
+++ app/controllers/api/v1/wardrobe_controller.rb
@@ -26,4 +26,23 @@ class Api::V1::WardrobeController < Api::ApiController
       return
     end
   end
+
+  def edit
+    if params[:authentication_token] != nil && params[:wardrobe] != nil
+      if User.find_by_authentication_token(authentication_token = params[:authentication_token])
+        @user = User.find_by_authentication_token(authentication_token = params[:authentication_token])
+        @user.wardrobe = params[:wardrobe]
+        @user.save!
+      else
+        logger.info("Failed connection to wardrobe json, a user cannot be found by that authentication_token.")
+        render :status =>200,
+               :json=>{:message=>"Failed wardrobe connection, a user cannot be found by that authentication_token."}
+      end
+    else
+      logger.info("Failed edit to wardrobe json, no authentication token posted or no wardrobe posted.")
+      render :status=>400,
+            :json=>{:message=>"Did you add the user's authentication_token?"}
+      return
+    end
+  end
 end
