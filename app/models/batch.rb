62308501fe0b588cb77618acfc1ab28193050319
Christina Leuci <christinaleuci@gmail.com>
2015-04-16 17:28:07 -0400

    changed json

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -8,19 +8,12 @@ class Batch < ActiveRecord::Base
       if obj.key =~ /swipe batches/
         if obj.key =~ /jpg/
           newfolder = sort_objs(obj.key)[0]
-          newbatch =  sort_objs(obj.key)[1]
           newurl = "https://s3.amazonaws.com/curateanalytics/" + obj.key.gsub('&', '%26').gsub('swipe ', 'swipe+')
           if Obj.key?(newfolder)
-            if Obj[newfolder].key?(newbatch)
-              Obj[newfolder].merge!(newbatch => [])
-              Obj[newfolder][newbatch] << {:filename => newurl, :properties => {}}
-              Obj[newfolder][newbatch].last[:properties] = find_properties(newurl, Obj[newfolder][newbatch].last[:properties])
-            else
-              Obj[newfolder].merge!(newbatch => {})
-            end
+            Obj[newfolder] << find_properties(newurl, Obj[newfolder]).first
           else
-            Obj.merge!(newfolder=>{})
-          end
+            Obj.merge!(newfolder => [])
+          end    
         end
       end
     end
@@ -34,27 +27,37 @@ class Batch < ActiveRecord::Base
     return [swipe, batch_id, file]
   end
 
-  def find_properties(url, properties_array)
+  def find_properties(url, properties_hash)
+    hash = {}
     s = File.join(Rails.root, 'public', 'DatabaseArray.json')
     file = File.read(s)
     data_hash = JSON.parse(file)
     data_hash.each do |main_category|
       main_category.each do |sub_category|
-        url = url.split("/").last.gsub("%26","&")
-        if (url == sub_category.split(",").first.split(":").second.split("\"").second)
-          array = sub_category.split(",")
-          array.each do |this|
-            name = this.split(":").first.split("\"").second
-            if (this.split(":").first.split("\"").second == "URL")
-              property = url
+        newurl = url.split("/").last.gsub("%26","&")
+        if (newurl == sub_category.split(",").first.split(":").second.split("\"").second)
+          sub_category.gsub("\"","")[1..-2].split(",").each do |string|
+            properties = string.split(":")
+            if (properties.first == "URL") || (properties.first == "File_Name")
+              if (properties.first == "URL")
+                properties_hash.last.merge!(properties.first.parameterize.underscore.to_sym => url)
+                properties_hash.last.merge!(:properties => {})
+              else
+                properties_hash << hash.merge!(properties.first.parameterize.underscore.to_sym => properties.second)
+              end
             else
-              property = this.split(":").second.split("\"").second
+              if properties_hash.last.key?(:properties)
+                if (properties.first == "Properties")
+                  properties_hash.last[:properties].merge!(properties.second.gsub!("{","").parameterize.underscore.to_sym => properties.last)
+                else
+                  properties_hash.last[:properties].merge!(properties.first.parameterize.underscore.to_sym => properties.second)
+                end
+              end
             end
-            properties_array.merge!(name => property)
           end
         end
       end  
     end
-  return properties_array
+  return properties_hash
   end
 end
--- app/models/wardrobe.rb
+++ app/models/wardrobe.rb
@@ -9,26 +9,36 @@ class Wardrobe < ActiveRecord::Base
   def serialize_wardrobe
     self.user_id = user.id
     self.authentication_token = user.authentication_token
-    self.wardrobe = {:tops => [{:filename => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Burgundy.jpg",
+    self.wardrobe = {:tops => [{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Burgundy.jpg",
       :properties => {}
-     },{:filename => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Charcoal.jpg",
+     },{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Charcoal.jpg",
       :properties => {}
-     },{:filename => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.DarkGreen.jpg",
+     },{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.DarkGreen.jpg",
       :properties => {}
-     },{:filename => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Gray.jpg",
+     },{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Gray.jpg",
       :properties => {}
-     },{:filename => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Green.jpg",
+     },{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/tops/hoodie/H%26M.Hoodie.Green.jpg",
       :properties => {}
      }],
-     :bottoms => [{:filename => "http://s3.amazonaws.com/curateanalytics/bottoms/pants/5pocket%20pants/regular/H&M_5-Pocket_Regular_Black.jpg",
+     :bottoms => [{:file_name => "",
+      :url => "http://s3.amazonaws.com/curateanalytics/bottoms/pants/5pocket%20pants/regular/H&M_5-Pocket_Regular_Black.jpg",
       :properties => {}
-     },{:filename => "http://s3.amazonaws.com/curateanalytics/bottoms/pants/5pocket%20pants/regular/H&M_5-Pocket_Regular_Blue.jpg",
+     },{:file_name => "",
+      :url => "http://s3.amazonaws.com/curateanalytics/bottoms/pants/5pocket%20pants/regular/H&M_5-Pocket_Regular_Blue.jpg",
       :properties => {}
-     },{:filename => "https://s3.amazonaws.com/curateanalytics/bottoms/shorts/twill/Uniqlo_Shorts_Stretch_Twill_Blue.jpg",
+     },{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/bottoms/shorts/twill/Uniqlo_Shorts_Stretch_Twill_Blue.jpg",
       :properties => {}
-     },{:filename => "https://s3.amazonaws.com/curateanalytics/bottoms/shorts/twill/Uniqlo_Shorts_Stretch_Twill_Brown.jpg",
+     },{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/bottoms/shorts/twill/Uniqlo_Shorts_Stretch_Twill_Brown.jpg",
       :properties => {}
-     },{:filename => "https://s3.amazonaws.com/curateanalytics/bottoms/shorts/twill/Uniqlo_Shorts_Stretch_Twill_Charcoal.jpg",
+     },{:file_name => "",
+      :url => "https://s3.amazonaws.com/curateanalytics/bottoms/shorts/twill/Uniqlo_Shorts_Stretch_Twill_Charcoal.jpg",
       :properties => {}
      }]}
      find_properties
@@ -43,17 +53,17 @@ class Wardrobe < ActiveRecord::Base
       main_category.each do |sub_category|
         self.wardrobe.keys.each do |key|
           self.wardrobe[key].each do |files|
-            @wardrobe_file_name = files[:filename].split("/").last.gsub("%26","&")
-            if (@wardrobe_file_name == sub_category.split(",").first.split(":").second.split("\"").second)
-              array = sub_category.split(",")
-              array.each do |this|
-                name = this.split(":").first.split("\"").second
-                if (this.split(":").first.split("\"").second == "URL")
-                  property = files[:filename]
-                else
-                  property = this.split(":").second.split("\"").second
+            @wardrobe_url = files[:url].split("/").last.gsub("%26","&")
+            if (@wardrobe_url == sub_category.split(",").first.split(":").second.split("\"").second)
+              sub_category.gsub("\"","")[1..-2].split(",").each do |string|
+                properties = string.split(":")
+                if (properties.first == "Properties")
+                  files[:properties].merge!(properties.second.gsub!("{","").parameterize.underscore.to_sym => properties.last)
+                elsif (properties.first == "File_Name")
+                  files[:file_name] = properties.second
+                elsif (properties.first != "URL")
+                  files[:properties].merge!(properties.first.parameterize.underscore.to_sym => properties.second)
                 end
-                files[:properties].merge!(name => property)
               end
             end
           end
