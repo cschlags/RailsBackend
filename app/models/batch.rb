84fd287a30655df65dc2ad742d0fce89c5a71365
Christina Leuci <christinaleuci@gmail.com>
2015-04-16 21:35:29 -0400

    batches

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,15 +2,25 @@ class Batch < ActiveRecord::Base
   serialize :folder, JSON
   Obj = {}
   def access_bucket
+    array = []
+    current = ""
     s3 = AWS::S3.new
     bucket = s3.buckets['curateanalytics']
     bucket.objects.each do |obj|
       if obj.key =~ /swipe batches/
         if obj.key =~ /jpg/
           newfolder = sort_objs(obj.key)[0]
+          newbatch = sort_objs(obj.key)[1]
           newurl = "https://s3.amazonaws.com/curateanalytics/" + obj.key.gsub('&', '%26').gsub('swipe ', 'swipe+')
           if Obj.key?(newfolder)
-            Obj[newfolder] << find_properties(newurl, Obj[newfolder]).first
+            if current == newbatch
+              array << find_properties(newurl)
+            else
+              current = newbatch
+              if array != []
+                Obj[newfolder] << array
+              end
+            end
           else
             Obj.merge!(newfolder => [])
           end    
@@ -27,7 +37,7 @@ class Batch < ActiveRecord::Base
     return [swipe, batch_id, file]
   end
 
-  def find_properties(url, properties_hash)
+  def find_properties(url)
     hash = {}
     s = File.join(Rails.root, 'public', 'DatabaseArray.json')
     file = File.read(s)
@@ -40,17 +50,17 @@ class Batch < ActiveRecord::Base
             properties = string.split(":")
             if (properties.first == "URL") || (properties.first == "File_Name")
               if (properties.first == "URL")
-                properties_hash.last.merge!(properties.first.parameterize.underscore.to_sym => url)
-                properties_hash.last.merge!(:properties => {})
+                hash.merge!(properties.first.parameterize.underscore.to_sym => url)
+                hash.merge!(:properties => {})
               else
-                properties_hash << hash.merge!(properties.first.parameterize.underscore.to_sym => properties.second)
+                hash.merge!(properties.first.parameterize.underscore.to_sym => properties.second)
               end
             else
-              if properties_hash.last.key?(:properties)
+              if hash.key?(:properties)
                 if (properties.first == "Properties")
-                  properties_hash.last[:properties].merge!(properties.second.gsub!("{","").parameterize.underscore.to_sym => properties.last)
+                  hash[:properties].merge!(properties.second.gsub!("{","").parameterize.underscore.to_sym => properties.last)
                 else
-                  properties_hash.last[:properties].merge!(properties.first.parameterize.underscore.to_sym => properties.second)
+                  hash[:properties].merge!(properties.first.parameterize.underscore.to_sym => properties.second)
                 end
               end
             end
@@ -58,6 +68,6 @@ class Batch < ActiveRecord::Base
         end
       end  
     end
-  return properties_hash
+  return hash
   end
 end
